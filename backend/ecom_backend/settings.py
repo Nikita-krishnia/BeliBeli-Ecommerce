d421578aab77cd3942a62f14456d822fb28bbbb7
Nikita <nikitakrishnia08@gmail.com>
2026-06-16 12:06:24 +0530

    feat: add celery broker and result backend settings

--- backend/ecom_backend/settings.py
+++ backend/ecom_backend/settings.py
@@ -174,3 +174,9 @@ STORAGES = {
         "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
     },  
 }
+
+
+CELERY_BROKER_URL = env('CELERY_BROKER_URL', default='redis://localhost:6379/0')
+CELERY_RESULT_BACKEND = env('CELERY_BROKER_URL', default='redis://localhost:6379/0')
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
