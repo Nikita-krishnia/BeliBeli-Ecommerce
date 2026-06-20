893426c87c4dda8cf493a57b5586929c3db738e6
Nikita <nikitakrishnia08@gmail.com>
2026-06-20 15:50:58 +0530

    fix: correct ALLOWED_HOSTS and CSRF_TRUSTED_ORIGINS for deployed render service

--- backend/ecom_backend/settings.py
+++ backend/ecom_backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['127.0.0.1', 'localhost'])
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['127.0.0.1', 'localhost', 'belibeli-ecommerce.onrender.com'])
 CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS', default=[])
 
 # Application definition
